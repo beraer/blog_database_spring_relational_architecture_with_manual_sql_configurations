56b09da111d3d05e9ad1ac59f3463b7a23d897a9
beraer <s29315@pjwstk.edu.pl>
2025-03-31 16:37:15 +0200

    project initialization

---
+++ src/main/java/myproject/s29315tpo04blog/S29315Tpo04BlogApplication.java
@@ -0,0 +1,13 @@
+package myproject.s29315tpo04blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S29315Tpo04BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S29315Tpo04BlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/myproject/s29315tpo04blog/S29315Tpo04BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package myproject.s29315tpo04blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S29315Tpo04BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
